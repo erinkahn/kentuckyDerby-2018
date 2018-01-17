6cc4dc273e7f3ca531c9d1a30b23481de7722436
erinkahn <erin.kahn@atlanta.creativecircus.edu>
2018-01-17 09:51:27 -0500

    first submit

---
+++ src/js/app.js
@@ -0,0 +1,46 @@
+
+(() => {
+	console.log("Hello World from app.js, transpiled and concatenated!");
+})();
+
+
+
+
+
+//HOME PAGE ANIMATION
+
+//when one of the boxes is clicked, disperse all the boxes
+
+// var allPages = document.querySelector('.row .col');
+// var horses = document.querySelector('.horses .col');
+// var timeline = document.querySelector('.timeline .col');
+// var facts = document.querySelector('.facts .col');
+// var fashion = document.querySelector('.fashion .col');
+// var rose = document.querySelector('.rose .col');
+// var mint = document.querySelector('.mint .col');
+
+// horses.onload = function() {
+
+// 	var tl = new TimelineMax();
+
+// 	tl.to(horses, 0.1, {top: 330});
+// }
+
+
+// for (var i = 0; i < allPages.length; i++) {
+// 	allPages[i].addEventListener('click', function(){
+		//disperse the boxes in diff directions
+		//create new class in css
+		//use tweenmax to move the boxes
+// 		horses.onload = function(){
+// 			setTimeout(function(e){
+// 				if (horses==clicked) {
+// 					e.preventDefault();
+// 					doAnimation();
+// 					linkToNewPage();
+// 				}
+// 			}, 100);
+// 		}
+// 	})
+// }
+
